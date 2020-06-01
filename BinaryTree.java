7127347955cb29d1bcd6aade7b481a9c1d9122cf
Kousthubha <kousthubha.ak@gmail.com>
2020-06-01 22:15:57 +0530

    Breadth first order traversal of binary tree
    3.Design a  binary tree and then print the breadth first order traversal of this tree. In breadth first order traversal, we visit the nodes of same height first then go to nodes of next height or level

---
+++ BinaryTree.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.util.Queue; 
+import java.util.LinkedList; 
+
+
+class Node { 
+	int data; 
+	Node left, right; 
+
+	public Node(int item) { 
+		data = item; 
+		left = null; 
+		right = null; 
+	} 
+} 
+
+
+class BinaryTree { 
+
+	Node root; 
+	void printLevelOrder() 
+	{ 
+		Queue<Node> queue = new LinkedList<Node>(); 
+		queue.add(root); 
+		while (!queue.isEmpty()) 
+		{ 
+
+			Node tempNode = queue.poll(); 
+			System.out.print(tempNode.data + " "); 
+
+			if (tempNode.left != null) { 
+				queue.add(tempNode.left); 
+			} 
+
+
+			if (tempNode.right != null) { 
+				queue.add(tempNode.right); 
+			} 
+		} 
+	} 
+
+	public static void main(String args[]) 
+	{ 
+		BinaryTree tree_level = new BinaryTree(); 
+		tree_level.root = new Node(1); 
+		tree_level.root.right = new Node(2); 
+		tree_level.root.right.right = new Node(5); 
+		tree_level.root.right.right.left = new Node(3); 
+		tree_level.root.right.right.right = new Node(6); 
+		tree_level.root.right.right.left.right = new Node(4); 
+
+		System.out.println("the level order traversal is - "); 
+		tree_level.printLevelOrder(); 
+	} 
+} 
